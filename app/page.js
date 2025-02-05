d22d7aa84c690b2c0ae2314696f0327c3a4928a4
Valentin Maystorovski <valentin.maystorovski@students.beaminstitute.org>
2025-02-05 15:36:45 +0100

    Moved header/navbar to layout

--- app/page.js
+++ app/page.js
@@ -4,43 +4,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header>
-        <div className="container">
-          <nav className="flex items-center justify-between py-4 flex-wrap">
-            <div>
-              <span className="font-medium text-xl">Musicfy</span>
-            </div>
-
-            <ul className="flex font-regular text-sm space-x-8">
-              <li>
-                <Link href="/trending">Trending</Link>
-              </li>
-              <li>
-                <Link href="/artists">Artists</Link>
-              </li>
-              <li>
-                <Link href="/pricing">Pricing</Link>
-              </li>
-              <li>
-                <Link href="/about-us">About us</Link>
-              </li>
-            </ul>
-
-            <div className="flex items-center space-x-8">
-              <Link className="text-sm" href="">
-                Log In
-              </Link>
-              <Link
-                className="font-medium text-sm bg-black text-white px-5 py-2 rounded-sm"
-                href=""
-              >
-                Get started
-              </Link>
-            </div>
-          </nav>
-        </div>
-      </header>
-
       <main>
         <section>
           <div className="container">
