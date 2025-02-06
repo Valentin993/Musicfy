75e47da8034313004a66821b627b232fc38aeb4f
Valentin Maystorovski <valentin.maystorovski@students.beaminstitute.org>
2025-02-06 16:58:37 +0100

    Added three cards

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
+import trendingStatistic from "../public/trending-statistic.svg";
+import artistAnalytics from "../public/artist-analytics.svg";
+import paasInsights from "../public/paas-insights.svg";
 
 export default function Home() {
   return (
@@ -9,8 +12,8 @@ export default function Home() {
           <div className="container px-4">
             <div className="flex flex-col items-center justify-center">
               <h1 className="font-medium text-5xl lg:text-6xl text-center mb-6 leading-snug">
-                <span className="block">Music Trends,Artists, and</span>
-                <span className="block">Insights at Your Fingertips.</span>
+                <span className="block">Music Trends, Artists, and</span>
+                <span className="block"> Insights at Your Fingertips.</span>
               </h1>
               <p className="text-base lg:text-lg text-center mb-8 max-w-[724px]">
                 Musicfy is your all-in-one platform for real-time music trends,
@@ -19,7 +22,7 @@ export default function Home() {
               </p>
               <Link
                 className="inline-block py-3 px-5 bg-black text-white rounded-sm font-medium"
-                href=""
+                href={""}
               >
                 Get Started for Free
               </Link>
@@ -28,16 +31,69 @@ export default function Home() {
         </section>
 
         <section className="features bg-black text-white py-24">
-          <div className="container px-4">
+          <div className="container lg:max-w-screen-lg px-4">
             <h2 className="font-medium text-5xl">
-              Statistics.Analytics.Insights
+              Statistics. Analytics. Insights.
             </h2>
-
             <p className="lg:w-6/12 mt-6">
               Musicfy is your all-in-one platform for real-time music trends,
               artist analytics, and actionable insights to stay ahead in the
               industry.
             </p>
+
+            <div className="mt-20 flex flex-col items-center md:flex-row flex-wrap">
+              <div className="card-container md:w-6/12">
+                <div className="card bg-[#ffffff05] shadow-inner-custom rounded-sm p-10 w-[380px] sm:w-[450px] md:w-[97%]">
+                  <h3 className="font-medium text-[32px]">
+                    Trending Statistics
+                  </h3>
+                  <p className="text-sm mt-2 mb-10">
+                    Track real-time music trends and discover
+                    <span className="block">whats hot in the industry.</span>
+                  </p>
+                  <Image
+                    src={trendingStatistic}
+                    alt=""
+                    className="w-[280px] md:w-auto md:h-[179px] lg:h-[227px]"
+                  />
+                </div>
+              </div>
+              <div className="card-container md:w-6/12 md:flex md:justify-end mt-7 md:mt-0">
+                <div className="card bg-[#ffffff05] shadow-inner-custom rounded-sm p-10 w-[380px] sm:w-[450px] md:w-[97%]">
+                  <h3 className="font-medium text-[32px]">Artist Analytics</h3>
+                  <p className="text-sm mt-2 mb-10">
+                    Dive deep into artist performance, top sellers,
+                    <span className="block">and fan engagement metrics.</span>
+                  </p>
+                  <Image
+                    src={artistAnalytics}
+                    alt=""
+                    className="w-[240px] md:w-auto"
+                  />
+                </div>
+              </div>
+
+              <div className="card-container mt-7 md:mt-5 lg:mt-7">
+                <div className="card bg-[#ffffff05] shadow-inner-custom rounded-sm p-10 w-[380px] sm:w-[450px] md:w-full md:flex md:justify-between md:items-center">
+                  <div>
+                    <h3 className="font-medium text-[32px]">
+                      Artist Analytics
+                    </h3>
+                    <p className="text-sm mt-2 mb-10 md:w-10/12">
+                      Gain exclusive insights into songs and artists with our
+                      powerful Platform-as-a-Service tools. Gain exclusive
+                      insights into songs and artists with our powerful
+                      Platform-as-a-Service tools.
+                    </p>
+                  </div>
+                  <Image
+                    src={paasInsights}
+                    alt=""
+                    className="lg:mr-24 w-[240px] md:w-auto"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </main>
